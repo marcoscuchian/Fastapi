1cf55d50f72f1f40efa04c2869c6762619d527ee
Marcos Cuchian <marcoscuchian@gmail.com>
2023-10-09 02:02:04 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ChallengeFastapi
+
+
+# Pasos a seguir para levantar el proyecto
+
+- Clonar repositorio.
+
+- Crear entorno virtual:
+
+``` python3 -m venv ./venv ```
+
+- Activar
+
+``` . venv/bin/activate ```
+
+- Instalar requirements
+
+``` pip install -r requirements.txt ```
+
+
+Dentro del proyecto, en la carpeta con
--- config/db.py
+++ config/db.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine, MetaData
 
-engine  = create_engine("mysql+pymysql://root:123456789@localhost:3306/clientes")
+user = "Your user here"
+password = "your password"
+
+engine  = create_engine(f"mysql+pymysql://{user}:{password}@localhost:3306/clientes")
 meta = MetaData()
 conn = engine.connect()
 
