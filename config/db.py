1a472968dba8a3808990030dc774c934d43b6d4f
Marcos Cuchian <marcoscuchian@gmail.com>
2023-10-10 17:32:30 -0300

    modif db

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-user = "your_uses"
+user = "your_user"
 password = "password"
 
 engine  = create_engine(f"mysql+pymysql://{user}:{password}@localhost:3306/challengeBanza")
